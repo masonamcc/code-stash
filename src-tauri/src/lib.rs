9d6a9567c47f40e34d09c8cd3e622c2b60d29a1f
Mason McCall <mccall.masona@gmail.com>
2026-02-04 21:06:57 -0500

    added plugin_fs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init()) // ✅ add this
+        .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             if cfg!(debug_assertions) {
